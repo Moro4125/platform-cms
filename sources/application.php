3567dda1f5a5cde4e76bcac640b0d1bf436e79e2
Andrey Morozkin <andrey.dmitrievich@gmail.com>
2017-12-03 12:38:55 +0300

    Version: 2.4.1

--- sources/application.php
+++ sources/application.php
@@ -30,7 +30,7 @@ class Application extends CApplication
 	use \Silex\Application\MonologTrait;
 	use \Silex\Application\FormTrait;
 
-	const PLATFORM_VERSION = "2.4.0";
+	const PLATFORM_VERSION = "2.4.1";
 
 	const SERVICE_CONTROLLERS_FACTORY = 'controllers_factory';
 	const SERVICE_DATABASE            = 'db';
